274cbe0d380bc77fcbaff1264a5aec09f06632e9
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-09-08 18:23:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 7-react-gallery-app
+ 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>What the freak is your freaking problem</h1>
+    </div>
+  );
+}
+
+export default App;
