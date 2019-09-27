9ba3377236fde9f1b40a47189794c20e8d92aaf1
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-09-27 16:02:15 -0500

    Navigation comp changes
    clear func resets button style
    makeActive func styles active button
    link default tag changes

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
 
+  const clear = (e) => {
+    if (e.target.nodeName === 'A') {
+      const allLI = e.target.parentNode.parentNode.children;
+      for (let i = 0; i < allLI.length; i++) {
+        allLI[i].firstElementChild.className = '';
+      }
+    }
+  }
+
+  const makeActive = (e) => {
+    clear(e);
+    if (e.target.nodeName === 'A') {
+      e.target.className = 'active';
+    }
+  }
+
   return (
+
     <nav className="main-nav">
-      <ul>
-        <li><Link to='/cats'>Cats</Link></li>
+      <ul onClick={ (e) => {makeActive(e)} }>
+        <li><Link to='/space'>Space</Link></li>
         <li><Link to='/nature'>Nature</Link></li>
-        <li><Link to='/computers'>Computers</Link></li>
+        <li><Link to='/buildings'>Buildings</Link></li>
+        <li><Link to='/cookies'>Cookies</Link></li>
+        <li><Link to='/cats'>Cats</Link></li>
+        <li><Link to='/jogging'>Jogging</Link></li>
       </ul>
     </nav>
   );
 }
 
-export default withRouter(Navigation);
+export default Navigation;
