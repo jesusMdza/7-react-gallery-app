7f43c954dd7ea6f069113fbc41e601e6da27b17f
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-09-27 16:03:53 -0500

    Photo.js space edit

--- src/Photo.js
+++ src/Photo.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Photo = ({ farm, serverID, id, secret, title }) => {
-
+ 
   return (
+    
     <li>
       <img src={`https://farm${farm}.staticflickr.com/${serverID}/${id}_${secret}.jpg`} alt={title} />
     </li>
