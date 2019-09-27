98156c3716394259ede8570fa65ccf91c9d2479c
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-09-27 16:06:54 -0500

    PhotoContainer.js
    conditionals added for mapped photos and photo container

--- src/PhotoContainer.js
+++ src/PhotoContainer.js
@@ -1,31 +1,63 @@
-import React from 'react';
-import Photo from './Photo';
+import React, { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
 
-const PhotoContainer = ({ data, getResults, match  }) => {
+import Photo from './Photo';
+import NoResults from './NoResults';
+import Spinner from './Spinner';
 
-  const tag = match.params.tag;
+class PhotoContainer extends PureComponent {
 
-  let photoKey = 1;
-  const photos = data.map(photo => 
-  <Photo 
-    farm={photo.farm}
-    serverID={photo.server}
-    id={photo.id}
-    secret={photo.secret}
-    title={photo.title}
-    key={photoKey++}
-  />
-  );
+  componentDidMount() {
+    const tag = this.props.match.params.tag;
+    const link = this.props.match.params.link;
+    const initialValue = tag !== undefined ? tag : link;
 
-  return (
-    <div className="photo-container">
-      <h2>Results</h2>
-      <ul>
-        {photos}
-      </ul>
-    </div>
-  );
+    this.props.getData(initialValue);
+  }
+
+  componentDidUpdate() {
+    const tag = this.props.match.params.tag;
+    const link = this.props.match.params.link;
+    const updatedValue = tag !== undefined ? tag : link;
+
+    this.props.getData(updatedValue);
+  }
+
+  render() {
+
+    let photoKey = 1;
+    const photos = this.props.data.map(photo => 
+      {
+        if (photoKey <= 24) {
+          return <Photo 
+          farm={photo.farm}
+          serverID={photo.server}
+          id={photo.id}
+          secret={photo.secret}
+          title={photo.title}
+          key={photoKey++}
+        />
+        } else if (photoKey > 24) {
+          return null;
+        }
+      }
+    );
+
+    if (this.props.isLoaded === true && this.props.data.length === 0) {
+      return ( <NoResults /> );
+    } else {
+      return (
+        <div className="photo-container">
+            { 
+              !this.props.isLoaded ? <h2>Loading</h2> : <h2>{this.props.value}</h2>
+            }
+            { 
+              !this.props.isLoaded ? <Spinner /> : <ul>{photos}</ul> 
+            }
+        </div>
+      );
+    }
+  }
 }
 
 export default withRouter(PhotoContainer);
