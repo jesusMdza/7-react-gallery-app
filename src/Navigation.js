a035124072526d5773ac8989d53ab540f457e528
Jesus Mendoza <jesuszmendoza14@gmail.com>
2019-10-03 22:21:38 -0500

    changed Link to NavLink JSX

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
 
-  const clear = (e) => {
-    if (e.target.nodeName === 'A') {
-      const allLI = e.target.parentNode.parentNode.children;
-      for (let i = 0; i < allLI.length; i++) {
-        allLI[i].firstElementChild.className = '';
-      }
-    }
-  }
-
-  const makeActive = (e) => {
-    clear(e);
-    if (e.target.nodeName === 'A') {
-      e.target.className = 'active';
-    }
-  }
-
   return (
-
     <nav className="main-nav">
-      <ul onClick={ (e) => {makeActive(e)} }>
-        <li><Link to='/space'>Space</Link></li>
-        <li><Link to='/nature'>Nature</Link></li>
-        <li><Link to='/buildings'>Buildings</Link></li>
-        <li><Link to='/cookies'>Cookies</Link></li>
-        <li><Link to='/cats'>Cats</Link></li>
-        <li><Link to='/jogging'>Jogging</Link></li>
+      <ul>
+        <li><NavLink to='/space' activeClassName='active'>Space</NavLink></li>
+        <li><NavLink to='/nature' activeClassName='active'>Nature</NavLink></li>
+        <li><NavLink to='/buildings' activeClassName='active'>Buildings</NavLink></li>
+        <li><NavLink to='/cookies' activeClassName='active'>Cookies</NavLink></li>
+        <li><NavLink to='/cats' activeClassName='active'>Cats</NavLink></li>
+        <li><NavLink to='/jogging' activeClassName='active'>Jogging</NavLink></li>
       </ul>
     </nav>
   );
